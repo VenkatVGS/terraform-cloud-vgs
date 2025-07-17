8d5414e68531255da946866597a1999c810e4e0b
VenkatVGS <venkat.tendulkar@gmail.com>
2025-07-17 12:46:54 +0530

    New Code

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+ # Adding Backend as S3 for Remote State Storage
+#   backend "s3" {
+#     bucket = "newkops7"
+#     key    = "devops/terraform.tfstate"
+#     region = "us-east-1"   
+#   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
